e7f1f62bd385c7ac0439b72065d15044ed666709
doananh234 <doananh234@gmail.com>
2018-11-21 00:10:48 +0700

    Update RNCarousel.m

--- ios/RNCarousel.m
+++ ios/RNCarousel.m
@@ -4,6 +4,7 @@
 #import <React/UIView+React.h>
 #import <React/RCTLog.h>
 #import "AudioToolbox/AudioServices.h"
+#import "RCTEventDispatcher.h"
 
 @implementation RNCarousel
 - (instancetype)init {
@@ -33,18 +34,14 @@
 //}
 
 
-//#pragma clang diagnostic push
-//#pragma clang diagnostic ignored "-Wobjc-missing-super-calls"
-// - (void)insertReactSubview:(id<RCTComponent>)subview atIndex:(NSInteger)atIndex {
-// //  [super insertReactSubview:subview atIndex:atIndex];
-  
-//    [self.items addObject:subview];
-  
-//    [self setCurrentItemIndex:0];
-//   [self reloadData];
-//   if (atIndex == 0) {
-//   }
-// }
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wobjc-missing-super-calls"
+//- (void)insertReactSubview:(id<RCTComponent>)subview atIndex:(NSInteger)atIndex {
+////  [super insertReactSubview:subview atIndex:atIndex];
+//
+//   [self.items addObject:subview];
+//
+//}
 
 - (void)didUpdateReactSubviews
 {
@@ -57,20 +54,39 @@
 }
 
 - (NSInteger)numberOfItemsInCarousel:(iCarousel *)carousel {
-  
   return [self.items count];
 }
 
 - (UIView *)carousel:(iCarousel *)carousel viewForItemAtIndex:(NSInteger)index reusingView:(UIView *)view {
-  NSString *path = [[NSBundle bundleWithIdentifier:@"com.apple.UIKit"] pathForResource:@"Tock" ofType:@"aiff"];
-  SystemSoundID soundID;
-  if (path != nil) {
-    AudioServicesCreateSystemSoundID((__bridge CFURLRef)[NSURL fileURLWithPath:path], &soundID);
-    AudioServicesPlaySystemSound(soundID);
-    AudioServicesDisposeSystemSoundID(soundID);
-    AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
-  }
+//  NSString *path = [[NSBundle bundleWithIdentifier:@"com.apple.UIKit"] pathForResource:@"Tock" ofType:@"aiff"];
+//  SystemSoundID soundID;
+//  if (path != nil) {
+//    AudioServicesCreateSystemSoundID((__bridge CFURLRef)[NSURL fileURLWithPath:path], &soundID);
+//    AudioServicesPlaySystemSound(soundID);
+//    AudioServicesDisposeSystemSoundID(soundID);
+//    AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+//  }
+//    if (view != nil) {
+//        return view;
+//    }
   return [self.items objectAtIndex:index];
 }
 
+#pragma mark -
+#pragma mark iCarousel taps
+
+- (void)carousel:(__unused iCarousel *)carousel didSelectItemAtIndex:(NSInteger)index
+{
+//    NSNumber *item = (self.items)[(NSUInteger)index];
+//    NSLog(@"Tapped view number: %@", item);
+    if (!self.onPressItem) {
+        return;
+    }
+    if (index == self.currentItemIndex) {
+//        NSDictionary *eventData = ;
+        self.onPressItem(@{
+                           @"index": @(self.currentItemIndex)
+                           });
+    }
+}
 @end
